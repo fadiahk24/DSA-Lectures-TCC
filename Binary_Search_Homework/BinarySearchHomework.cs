66a370eafe398f95496eeb000d13484d174ee51b
fadiahk24 <150503327+fadiahk24@users.noreply.github.com>
2023-12-19 10:25:06 +0300

    Update BinarySearchHomework.cs

--- Binary_Search_Homework/BinarySearchHomework.cs
+++ Binary_Search_Homework/BinarySearchHomework.cs
@@ -14,6 +14,26 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                                 // من الممكن تمرير قيمة 0 لتجاهلهم
         {
             //TODO: write code for the ternary search algorithm and return the index of the element
+            if (end >= start)
+            {
+                int firstmid = start + (end - start) / 3;
+                int secondmid = end - (end - start) / 3;
+
+                if (ar[firstmid] == key)
+                    return firstmid;
+
+                if (ar[secondmid] == key)
+                    return secondmid;
+
+                if (key < ar[firstmid])
+                    return TernarySearch(ar, key, start, firstmid - 1);
+
+                else if (key > ar[secondmid])
+                    return TernarySearch(ar, key, secondmid + 1, end);
+
+                else
+                    return TernarySearch(ar, key, firstmid + 1, secondmid - 1);
+            }
             return -1;
         }
 
@@ -24,14 +44,60 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                     // من الممكن تمرير قيمة 0 لتجاهلهم
         {
             //TODO: this methods is for getting the first accurence of the key and the last accurance
-            return -1;
+          int result = -1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (array[mid] == target)
+                {
+                    result = mid;
+                    if (is_first)
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                else if (array[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+                if (!is_first && mid == array.Length - 1) break;
+
+            }
+
+            return result;
         }
 
         public static int GetRepeatCount(int[] arr, int key) // don't edit this line!!!
         {
             //TODO: write code to calculate the repeat count of a spacific element
             // make sure to use the previous method in this method
-            return -1;
+          
+            int first = BinarySearchForCalculatingRepeated(arr, key, true, 0, arr.Length);
+            int last = BinarySearchForCalculatingRepeated(arr, key, false, 0, arr.Length);
+            if (first == -1 || last == -1)
+            {
+                return 0;
+            }
+
+            int count = 0;
+            for (int i = first; i <= last; i++)
+                if (key == arr[i])
+                    count++;
+
+            if (count > 0)
+                return count;
+            else
+                return 0;
         }
     }
 }
